70eac554b7fd3fabef5d8850ce2ac3292c273213
id935jd3 <id935jd3@boun.cr>
2020-07-10 17:53:04 +0200

    Moved javascript to separate file

---
+++ denhaag.js
@@ -0,0 +1,21 @@
+var mymap = L.map('mapid').setView([52.077396, 4.31247], 14);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoiaWQ5MzVqZDMiLCJhIjoiY2tjZGp1M3VxMDBkbTJxc3dmbTY1ZWhlcCJ9.w3laGzgZR7en_xEpKkc-Og'
+}).addTo(mymap);
+
+var popup = L.popup();
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(mymap);
+}
+
+mymap.on('click', onMapClick);
