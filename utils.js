b4d68c80b85312cf4beb53d8b78b7262dd9c90dc
id935jd3 <id935jd3@boun.cr>
2020-07-18 15:46:29 +0200

    Added language support

--- utils.js
+++ utils.js
@@ -25,3 +25,21 @@ function animateCollapsible() {
         });
     }
 }
+
+function localize(language) {
+    const supportedLanguages = ["en", "nl"];
+    let supportedLanguage;
+    for (supportedLanguage of supportedLanguages) {
+        if ([supportedLanguage].includes(language)) {
+            let lang = ':lang(' + language + ')';
+            let hide = '[lang]:not(' + lang + ')';
+            document.querySelectorAll(hide).forEach(function (node) {
+                node.style.display = 'none';
+            });
+            let show = '[lang]' + lang;
+            document.querySelectorAll(show).forEach(function (node) {
+                node.style.display = 'unset';
+            });
+        }
+    }
+}
