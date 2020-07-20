25a0f5877cde788b21275ff26c09eb2ed4e4a36c
id935jd3 <id935jd3@boun.cr>
2020-07-20 12:36:54 +0200

    Added location for zeeheldenkwartier

--- neighbourhood.js
+++ neighbourhood.js
@@ -1,33 +1,4 @@
-let neighbourhood1PopupContent = readTextFileAsString("location1PopupContent.html");
-
-const neighbourhood1 = {
-    "type": "Feature",
-    "properties": {
-        "popupContent": neighbourhood1PopupContent,
-        "style": {
-            weight: 2,
-            color: "#999",
-            opacity: 1,
-            fillColor: "#FF6666",
-            fillOpacity: 0.7
-        }
-    },
-    "geometry": {
-        "type": "MultiPolygon",
-        "coordinates": [
-            [
-                [
-                    [4.315845966339112, 52.08624414714523],
-                    [4.312820434570313, 52.089975401365436],
-                    [4.327261447906495, 52.09510371866937],
-                    [4.330050945281983, 52.09237484171005]
-                ]
-            ]
-        ]
-    }
-};
-
-const neighbourhoodList = [neighbourhood1];
+const neighbourhoodList = [zeeheldenkwartier];
 
 let location1PopupContent = readTextFileAsString("location1PopupContent.html");
 
---
+++ neighbourhoods/gentrifying/zeeheldenkwartier.js
@@ -0,0 +1,36 @@
+let popupContent = readTextFileAsString("neighbourhoods/gentrifying/zeeheldenkwartierPopup.html");
+
+const zeeheldenkwartier = {
+    "type": "Feature",
+    "properties": {
+        "popupContent": popupContent,
+        "style": {
+            weight: 2,
+            color: "#999",
+            opacity: 1,
+            fillColor: "#FF6666",
+            fillOpacity: 0.7
+        }
+    },
+    "geometry": {
+        "type": "MultiPolygon",
+        "coordinates": [
+            [
+                [
+                    [4.294775, 52.076736],
+                    [4.288015, 52.081589],
+                    [4.293466, 52.084543],
+                    [4.293187, 52.085037],
+                    [4.293562, 52.085222],
+                    [4.298519, 52.086527],
+                    [4.298830, 52.087121],
+                    [4.300665, 52.086831],
+                    [4.303905, 52.083996],
+                    [4.301115, 52.082895],
+                    [4.303358, 52.080211],
+                    [4.294817, 52.076624]
+                ]
+            ]
+        ]
+    }
+};
