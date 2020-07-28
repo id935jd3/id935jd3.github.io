751c62c4db0e708888b4c0d6bb7fe23fe4296025
id935jd3 <id935jd3@boun.cr>
2020-07-28 16:08:56 +0200

    Added vogelwijk

--- neighbourhood.js
+++ neighbourhood.js
@@ -1,7 +1,7 @@
 const notGentrifiedNeighbourhoods = [transvaal];
 const gentrifyingNeighbourhoods = [zeeheldenkwartier, moerwijk, binckhorst, duindorp, schilderswijk,
     regentesseValkenboskwartier, scheveningenHaven];
-const richNeighbourhoods = [duinoord, archipelbuurt];
+const richNeighbourhoods = [duinoord, archipelbuurt, vogelwijk];
 
 const neighbourhoodList = notGentrifiedNeighbourhoods.concat(gentrifyingNeighbourhoods, richNeighbourhoods);
 
